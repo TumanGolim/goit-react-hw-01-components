4f22d1eb14306a118a381cd05181f8d97279eb2b
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-11-28 14:53:36 +0200

    fix friendList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './FriendList.module.css';
+import FriendListItem from './FriendListItem'; 
+import css from './FriendList.module.css';
 
-const FriendList = ({ friends }) => (
-  <ul className={styles.friendList}>
-    {friends.map(({ id, avatar, name, isOnline }) => (
-      <li key={id} className={styles.item}>
-        <span className={isOnline ? styles.online : styles.offline}></span>
-        <img
-          src={avatar}
-          alt={`User avatar ${name}`}
-          className={styles.avatar}
-          width="48"
+export default function FriendList({ friends }) {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
         />
-        <p className={styles.name}>{name}</p>
-      </li>
-    ))}
-  </ul>
-);
+      ))}
+    </ul>
+  );
+}
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
     })
   ).isRequired,
 };
-
-export default FriendList;
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css'; 
+
+export default function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <li className={css.item}>
+      <span
+        className={css.status}
+        style={{
+          backgroundColor: isOnline ? 'green' : 'red',
+        }}
+      ></span>
+      <img
+        className={css.avatar}
+        src={avatar}
+        alt={`User avatar ${name}`}
+        width="48"
+      />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
